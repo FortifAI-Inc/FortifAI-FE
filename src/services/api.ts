9ab85b57c334419c6d1e5a4b98191a6b626deff0
Hilik Stein <hilik.stein@gmail.com>
2025-04-11 17:00:07 +0300

    fix is_stale handling

--- src/services/api.ts
+++ src/services/api.ts
@@ -250,6 +250,7 @@ export class ApiService {
               is_ai: asset.metadata?.is_ai || asset.metadata?.is_ai,
               ai_detection_details: asset.metadata?.ai_detection_details || asset.metadata?.ai_detection_details,
               is_ignored: asset.metadata?.is_ignored || asset.metadata?.is_ignored,
+              is_stale: asset.is_stale,
               tags: asset.tags,
               is_sandbox: asset.metadata?.is_sandbox || asset.metadata?.is_sandbox
             }
